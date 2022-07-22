217ddf8c4d0f0681743b651e4d74115084bc43e0
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2022-07-22 16:54:28 +0900

    Add files via upload

---
+++ 3-2-4-1 helloworld.c
@@ -0,0 +1,10 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 3 , Semester 2 , Chapter 4 , Number 1 */
+
+#include <stdio.h> // 표준입출력 헤더파일 포함
+
+int main(void) // 메인 함수
+{
+    printf("Hello World\n"); // "Hello World" 문장을 모니터에 출력
+    return 0;
+}
